e0b457bde75571c7ca16720714f7749bc0e846ae
Ruslan Kyrychuk <ruslan.kyrychuk@gmail.com>
2019-07-12 13:14:37 +0300

    cleanup

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-// [START get_messaging_object]
 const messaging = firebase.messaging();
-
-// IDs of divs that display Instance ID token UI or request permission UI.
-const tokenDivId = 'token_div';
-const permissionDivId = 'permission_div';
-// [START refresh_token]
-// Callback fired if Instance ID token is updated.
 messaging.onTokenRefresh(() => {
   messaging.getToken().then((refreshedToken) => {
-    console.log('Token refreshed.');
     setTokenSentToServer(false);
     sendTokenToServer(refreshedToken);
     resetUI();
   }).catch((err) => {
-    console.log('Unable to retrieve refreshed token ', err);
     showToken('Unable to retrieve refreshed token ', err);
   });
 });
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.setBackgroundMessageHandler` handler.
 messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);
   appendMessage(payload);
 })
 
@@ -32,16 +18,11 @@ function resetUI() {
   messaging.getToken().then((currentToken) => {
     if (currentToken) {
       sendTokenToServer(currentToken);
-      updateUIForPushEnabled(currentToken);
     } else {
-      // Show permission request.
-      console.log('No Instance ID token available. Request permission to generate one.');
-      // Show permission UI.
-      updateUIForPushPermissionRequired();
+      showToken('No Instance ID token available. Request permission to generate one.');
       setTokenSentToServer(false);
     }
   }).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
     showToken('Error retrieving Instance ID token. ' + err);
     setTokenSentToServer(false);
   });
@@ -56,11 +37,7 @@ function showToken(currentToken) {
 function sendTokenToServer(currentToken) {
   if (!isTokenSentToServer()) {
     console.log('Sending token to server...');
-    // TODO(developer): Send the current token to your server.
     setTokenSentToServer(true);
-  } else {
-    console.log('Token already sent to server so won\'t send it again ' +
-        'unless it changes');
   }
 }
 function isTokenSentToServer() {
@@ -69,22 +46,11 @@ function isTokenSentToServer() {
 function setTokenSentToServer(sent) {
   window.localStorage.setItem('sentToServer', sent ? '1' : '0');
 }
-function showHideDiv(divId, show) {
-  const div = document.querySelector('#' + divId);
-  if (show) {
-    div.style = 'display: visible';
-  } else {
-    div.style = 'display: none';
-  }
-}
+
 function requestPermission() {
-  console.log('Requesting permission...');
   Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
-      console.log('Notification permission granted.');
       resetUI();
-    } else {
-      console.log('Unable to get permission to notify.');
     }
   });
 }
@@ -93,15 +59,13 @@ function deleteToken() {
   // [START delete_token]
   messaging.getToken().then((currentToken) => {
     messaging.deleteToken(currentToken).then(() => {
-      console.log('Token deleted.');
       setTokenSentToServer(false);
       resetUI();
     }).catch((err) => {
       console.log('Unable to delete token. ', err);
     });
   }).catch((err) => {
-    console.log('Error retrieving Instance ID token. ', err);
-    showToken('Error retrieving Instance ID token. ', err);
+    showToken('Error retrieving Instance ID token. ' + err);
   });
 }
 // Add a message to the messages element.
@@ -122,13 +86,5 @@ function clearMessages() {
     messagesElement.removeChild(messagesElement.lastChild);
   }
 }
-function updateUIForPushEnabled(currentToken) {
-  showHideDiv(tokenDivId, true);
-  showHideDiv(permissionDivId, false);
-  showToken(currentToken);
-}
-function updateUIForPushPermissionRequired() {
-  showHideDiv(tokenDivId, false);
-  showHideDiv(permissionDivId, true);
-}
+
 resetUI();
